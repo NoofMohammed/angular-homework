d59cc5ba87a69bc1d9982cf31186fcfcaa2ed2f2
Noof <noof.mohammed998@gmail.com>
2021-08-26 11:54:02 +0300

    style

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,7 +10,7 @@ export class HomeComponent implements OnInit {
   newTodo: string = '';
   todoUpdated: string = '';
   todos: string[] = ['study', 'read', 'cook'];
-  inputUpdata: Boolean = false
+  inputUpdata: Boolean = false;
 
   addTodo(newTodo: string): String[] {
     this.todos.push(newTodo);
@@ -18,33 +18,29 @@ export class HomeComponent implements OnInit {
     return this.todos;
   }
   deleteTodo(todo: string): String[] {
-    this.todos.splice(this.todos.indexOf(todo),1);
+    this.todos.splice(this.todos.indexOf(todo), 1);
     console.log(this.todos, 'lllllll');
     return this.todos;
   }
 
-TodoEmpty(): boolean {
-  console.log(this.todos);
-if(this.todos.length === 0){
+  TodoEmpty(): boolean {
+    console.log(this.todos);
+    if (this.todos.length === 0) {
+      return true;
+    }
+    return false;
+  }
+  updateTodo(todo: string, todoUpdated: string): String[] {
+    console.log(this.todos[this.todos.indexOf(todo)]);
+    if (this.todos.indexOf(todo) !== -1) {
+      this.todos[this.todos.indexOf(todo)] = todoUpdated;
+    }
 
-  return true
-}
-return false
-
-}
-updateTodo(todo: string , todoUpdated: string): String[] {
-
-  console.log(this.todos[this.todos.indexOf(todo)]);
-  if(this.todos.indexOf(todo) !== -1){
-    this.todos[this.todos.indexOf(todo)]= todoUpdated
+    return this.todos;
   }
 
-  return this.todos
-}
-
-
- arrayEmpty: boolean = this.TodoEmpty()
- buttonColor: String = "green"
+  arrayEmpty: boolean = this.TodoEmpty();
+  buttonColor: String = 'green';
   constructor() {}
 
   ngOnInit(): void {}
